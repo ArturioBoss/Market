126b66a5d56ceecd7e948328b5d48ddff6363845
Artur <arturiosboss>
2021-08-03 16:47:52 +0300

    Lession_5

--- src/main/java/com/arturios/market/controllers/AdminController.java
+++ src/main/java/com/arturios/market/controllers/AdminController.java
@@ -4,12 +4,14 @@ import com.arturios.market.entites.Order;
 import com.arturios.market.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,6 +26,13 @@ public class AdminController {
     @GetMapping
     public String showAdminDashboard() {
         return "admin-panel";
+    }
+
+    @GetMapping("/orders")
+    public String showOrders(Model model) {
+        List<Order> orders = orderService.getAllOrders();
+        model.addAttribute("orders", orders);
+        return "orders-page";
     }
 
     @GetMapping("/orders/ready/{id}")
--- src/main/java/com/arturios/market/controllers/ProductController.java
+++ src/main/java/com/arturios/market/controllers/ProductController.java
@@ -1,11 +1,19 @@
 package com.arturios.market.controllers;
 
+import com.arturios.market.entites.Product;
+import com.arturios.market.entites.ProductImage;
 import com.arturios.market.services.CategoryService;
 import com.arturios.market.services.ImageSaverService;
 import com.arturios.market.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -27,5 +35,40 @@ public class ProductController {
     @Autowired
     public void setImageSaverService(ImageSaverService imageSaverService) {
         this.imageSaverService = imageSaverService;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable(name = "id") Long id) {
+        Product product = productService.getProductById(id);
+        if (product == null) {
+            product = new Product();
+            product.setId(0L);
+        }
+        model.addAttribute("product", product);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "/edit-product";
+    }
+
+    @PostMapping("/edit")
+    public String processProductAddForm(@Valid @ModelAttribute("product") Product product, BindingResult theBindingResult, Model model, @RequestParam("file") MultipartFile file) {
+        if (product.getId() == 0 && productService.isProductWithTitleExists(product.getTitle())) {
+            theBindingResult.addError(new ObjectError("product.title", "Товар с таким названием уже существует")); // todo не отображает сообщение
+        }
+
+        if (theBindingResult.hasErrors()) {
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "edit-product";
+        }
+
+        if (!file.isEmpty()) {
+            String pathToSavedImage = imageSaverService.saveFile(file);
+            ProductImage productImage = new ProductImage();
+            productImage.setPath(pathToSavedImage);
+            productImage.setProduct(product);
+            product.addImage(productImage);
+        }
+
+        productService.saveProduct(product);
+        return "redirect:/shop";
     }
 }
